fc81961ad4f44707bac40cd2fe0f159a9964a9cb
Giorgos Kontopoulos <kongeo@gmail.com>
2018-06-08 19:01:38 +0300

    changed api_list.php with only working servers

--- api_list.php
+++ api_list.php
@@ -51,8 +51,8 @@ $context = stream_context_create($opts);
                         
 $domains = [
   //"http://104.251.218.154:9998/api?", /* anoxy 2nd */
-  "http://79.137.38.49:9998/api?", /* giorgosk 1st */
   "http://195.181.242.206:9998/api?", /* anoxy 1st */  
+  "http://79.137.38.49:9998/api?", /* giorgosk 1st */
   //"http://63.142.250.11:9998/api?", /* giorgosk 2nd */
 ];
 
